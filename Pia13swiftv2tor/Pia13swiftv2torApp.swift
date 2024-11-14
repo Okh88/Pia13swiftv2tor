8a1aefeddf5e1f2849d47ad5de722dfd0af3363c
Okh88 <orwah.khadrow@gmail.com>
2024-11-14 09:14:32 +0100

    Initial Commit

---
+++ Pia13swiftv2tor/Pia13swiftv2torApp.swift
@@ -0,0 +1,17 @@
+//
+//  Pia13swiftv2torApp.swift
+//  Pia13swiftv2tor
+//
+//  Created by Malmö Yrkehögskolan on 2024-11-14.
+//
+
+import SwiftUI
+
+@main
+struct Pia13swiftv2torApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
